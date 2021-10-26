1a6625fcc82d96ad041ac0a73b5413581c14a4b6
umarfaraz <umarfaraz005@gmail.com>
2021-10-26 09:10:50 +0500

    initial commit
    push the complete signleton design pattern cod

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Signleton signleton = Signleton.GetInstance;
+            signleton.PrintDetails("This is first signleton object");
+           
+
+            Signleton developer = Signleton.GetInstance;
+            developer.PrintDetails("developer");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Signleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public sealed class Signleton
+    {
+       private static int Counter = 0;
+       private static Signleton instenace = null;
+       public static Signleton GetInstance
+        {
+            get
+            { if (instenace == null)
+                    instenace = new Signleton();
+                return instenace;
+               
+            }
+        }
+
+
+       private  Signleton()
+        {
+            Counter++;
+            Console.WriteLine("this is constructor"+Counter.ToString());
+
+
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+  
+}
